320ef44cc254d24f283f7ed0eb5e100b0b421402
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-02 23:26:45 +0900

    3주차: 서버통신 - RecyclerView Adapter 서버통신

--- app/src/main/java/com/teambeme/bemeroid/adapter/ProjectAdapter.kt
+++ app/src/main/java/com/teambeme/bemeroid/adapter/ProjectAdapter.kt
@@ -1,20 +1,25 @@
 package com.teambeme.bemeroid.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.teambeme.bemeroid.databinding.ItemProjectInfoBinding
 import com.teambeme.bemeroid.model.ProjectData
+import com.teambeme.bemeroid.model.ResponsePeople
 
 class ProjectAdapter : RecyclerView.Adapter<ProjectAdapter.ProjectViewHolder>() {
-    private var projectList = listOf<ProjectData>()
-    class ProjectViewHolder(private val binding: ItemProjectInfoBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(projectData: ProjectData) {
+    private var projectList = listOf<ResponsePeople.Data>()
+    class ProjectViewHolder(
+        private val binding: ItemProjectInfoBinding,
+        private val context: Context
+        ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(peopleData: ResponsePeople.Data) {
             with(binding) {
-                imgProjectInfo.setImageResource(projectData.imgProject)
-                txtProjectTitle.text = projectData.txtProjectTitle
-                txtProjectContent.text = projectData.txtProjectContents
+                Glide.with(context).load(peopleData.avatar).into(binding.imgProjectInfo)
+                txtProjectTitle.setText("${peopleData.firstName} ${peopleData.lastName}")
+                txtProjectContent.text = peopleData.email
             }
         }
     }
@@ -22,7 +27,7 @@ class ProjectAdapter : RecyclerView.Adapter<ProjectAdapter.ProjectViewHolder>() 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = ItemProjectInfoBinding.inflate(layoutInflater, parent, false)
-        return ProjectViewHolder(binding)
+        return ProjectViewHolder(binding, parent.context)
     }
 
     override fun onBindViewHolder(holder: ProjectViewHolder, position: Int) {
@@ -31,7 +36,7 @@ class ProjectAdapter : RecyclerView.Adapter<ProjectAdapter.ProjectViewHolder>() 
 
     override fun getItemCount() = projectList.size
 
-    fun replaceList(list: List<ProjectData>) {
+    fun replaceList(list: List<ResponsePeople.Data>) {
         projectList = list.toList()
         notifyDataSetChanged()
     }
